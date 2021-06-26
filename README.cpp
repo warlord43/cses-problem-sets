bb1f9a30a6e3627478bb1a3a6365164758e55c74
Yashas Kirnapure <84733649+warlord43@users.noreply.github.com>
2021-06-26 19:52:11 +0530

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,26 @@
+
+
+//1.missing number:You are given all numbers between 1,2,…,n except one. Task is to find missing number.
+#include <iostream>
+using namespace std;
+int main() {
+  int arr[]={1,2,3,4,5};
+  int arr_modified[]={1,2,4,5};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  int i=0;
+   int j;
+ // cout<<n;
+  while(i<n){
+   
+     for(j=0;j<n-1;j++){
+       if(arr[i]==arr_modified[j]){
+         i++;
+        break;
+       }
+     }if(j==n-1){
+       cout<<arr[i]<<endl;
+       i++;
+     }
+   
+  }
+}
