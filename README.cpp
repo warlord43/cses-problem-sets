4baa430886248bd1fffb9dd11b47680ac8b373ac
Yashas Kirnapure <84733649+warlord43@users.noreply.github.com>
2021-06-26 19:58:18 +0530

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,21 @@
+//2.You are given a DNA sequence: a string consisting of characters A, C, G, and T. Your task is to find the longest repetition in the sequence. This is a maximum-length substring containing only one type of character.
+#include<iostream>
+using namespace std;
+#include<algorithm>
+
+int main(){
+  string s;
+  cin>>s;    //only A,G,C,T allowed
+ int n=s.size();
+ int i=0;
+ int count=1;
+ int result;
+ for(i=0;i<n;i++){
+   if(s[i]==s[i+1]){
+     count++;
+     i++;
+   }
+   result=max(result,count);
+ }
+ cout<<result;
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# cses-problem-sets
-
-1.missing number:You are given all numbers between 1,2,…,n except one. Task is to find missing number.
